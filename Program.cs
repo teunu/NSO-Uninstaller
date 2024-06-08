5fa1024c1507722add6f34bfd7be2ce446165dae
CodeLeopard <gitlab@Codeleopard.net>
2024-06-08 17:35:38 +0200

    Check we are in the correct folder before running.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Pastel;
+using Pastel;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -9,6 +9,7 @@ const string ARG_NO_HEADER = "--no-header";
 const string ARG_DRY_RUN = "--dry-run";
 const string ARG_SKIP_CONFIRM = "--confirm";
 const string ARG_ONLY_STARTUP = "--only-startup";
+const string ARG_SKIP_EXIT_KEYPRESS = "--skip-exit-keypress";
 
 string root = AppContext.BaseDirectory;
 
@@ -20,7 +21,8 @@ if (! args.Contains(ARG_NO_HEADER))
 
 	Console.WriteLine
 	(
-		"Copyright 2024 teunu, CodeLeopard"
+		"NSO Uninstaller"
+		+ "\nCopyright 2024 teunu, CodeLeopard"
 		+ "\nThis program comes with ABSOLUTELY NO WARRANTY"
 		//+ "\nLicense: LGPL-3.0-or-later See: https://www.gnu.org/licenses/" // todo: pick license and add to repo.
 		+ "\nSource code: https://github.com/teunu/NSO-Uninstaller"
@@ -31,6 +33,7 @@ if (! args.Contains(ARG_NO_HEADER))
 
 bool dryRun = args.Contains(ARG_DRY_RUN);
 bool skipConfirmation = args.Contains(ARG_SKIP_CONFIRM);
+bool skipExitKeyPress = args.Contains(ARG_SKIP_EXIT_KEYPRESS);
 
 
 if (dryRun)
@@ -38,6 +41,16 @@ if (dryRun)
 
 if (skipConfirmation)
 	Console.WriteLine($"Arg: {ARG_SKIP_CONFIRM} -> Skipping prompts for confrmation.");
+
+var stormworksExepath = Path.Combine(root, "stormworks.exe");
+var stormworksExepath64 = Path.Combine(root, "stormworks64.exe");
+if (!File.Exists(stormworksExepath) && !File.Exists(stormworksExepath64))
+{
+	Console.WriteLine($"Did not find '{stormworksExepath}' unable to uninstall NSO.".Pastel(ConsoleColor.Red));
+	Console.WriteLine("The uninstaller should be placed in the stormworks installation folder, next to 'stormworks.exe'.".Pastel(ConsoleColor.Red));
+	Finish();
+	return 1;
+}
 
 Console.WriteLine($"About to uninstall NSO from: {root}");
 
@@ -68,6 +81,7 @@ void Uninstall()
 	// The script that generates the installation zip file also creates an inventory of files.
 	// We simply delete all the files mentioned there.
 	// Note: If an update to NSO stops including a file it would be left behind by the uninstaller, so the updater should remove it at update time.
+	// Note: This file is also on the list to be deleted, so we have to read it all and close it before starting.
 	var lines = File.ReadAllLines(Path.Combine(root, "rom/nso_mod/files.txt"));
 	foreach (var line in lines)
 	{
@@ -86,18 +100,22 @@ void Uninstall()
 			}
 			Console.WriteLine(finalPath.Pastel(ConsoleColor.Gray));
 		}
-		catch (DirectoryNotFoundException)
+		catch (DirectoryNotFoundException ex)
 		{
-			// Ignore.
+			Debug.WriteLine(line);
+			Debug.WriteLine(ex.ToString());
 		}
-		catch (FileNotFoundException)
+		catch (FileNotFoundException ex)
 		{
-			// Ignore
+			Debug.WriteLine(line);
+			Debug.WriteLine(ex.ToString());
 		}
 		catch (Exception ex)
 		{
-			Console.WriteLine($"Failed to delete '{line}'.".Pastel(ConsoleColor.Yellow));
+			Debug.WriteLine(line);
 			Debug.WriteLine(ex.ToString());
+
+			Console.WriteLine($"Failed to delete '{line}'.".Pastel(ConsoleColor.Yellow));
 		}
 	}
 
@@ -132,6 +150,9 @@ void Uninstall()
 
 void Finish()
 {
+	if (skipExitKeyPress)
+		return;
+
 	Console.WriteLine("Waiting for keypress before closing...");
 	Console.ReadKey();
 }
