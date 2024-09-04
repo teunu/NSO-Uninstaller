1b3bc212c3ad2fed238140e6b4031ae9fa764489
CodeLeopard <gitlab@Codeleopard.net>
2024-09-04 23:28:43 +0200

    Improvements

---
+++ ModInfo.cs
@@ -0,0 +1,58 @@
+﻿using Pastel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace NSO_Uninstaller;
+
+internal class ModInfo
+{
+	public string short_code => properties["short_code"];
+	public string short_name => properties["short_name"];
+	public string full_name => properties["full_name"];
+	public string version => properties["version"];
+
+	public readonly Dictionary<string, string> properties = new()
+	{
+		{ "short_code", "???" },
+		{ "short_name", "???" },
+		{ "full_name", "Unknown" },
+		{ "version", "v?.?.?" },
+	};
+	public readonly List<string> file_list = new();
+
+	public static ModInfo Parse(string filePath)
+	{
+		// The script that generates the installation zip file also creates an inventory of files.
+		// So to uninstall we simply delete all the files mentioned there.
+		// Note: If an update to NSO stops including a file it would be left behind by the uninstaller, so the updater should remove it at update time.
+		// Note: This file is also on the list to be deleted, so we have to read it all and close it before starting.
+		var lines = File.ReadAllLines(filePath);
+
+		var modInfo = new ModInfo();
+
+		foreach (var line in lines)
+		{
+			if (string.IsNullOrWhiteSpace(line) || line.StartsWith('#'))
+			{
+				continue;
+			}
+
+			const string propertyHeader = "!property ";
+			if (line.StartsWith(propertyHeader))
+			{
+				var key = line.Substring(propertyHeader.Length).Split(' ').First();
+				var value = line.Substring(propertyHeader.Length + key.Length + 1 /* separator after key */);
+
+				modInfo.properties[key] = value;
+				continue;
+			}
+
+			modInfo.file_list.Add(line);
+		}
+
+		return modInfo;
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Pastel;
+﻿using NSO_Uninstaller;
+using Pastel;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -6,23 +7,29 @@ using System.Linq;
 
 const bool FORCE_VALIDATION = false;
 
+const string ARG_WAIT_FOR_DEBUGGER = "--wait-for-debugger";
 const string ARG_NO_HEADER = "--no-header";
 const string ARG_DRY_RUN = "--dry-run";
 const string ARG_SKIP_CONFIRM = "--confirm";
 const string ARG_ONLY_STARTUP = "--only-startup";
 const string ARG_SKIP_EXIT_KEYPRESS = "--skip-exit-keypress";
 
-string root = AppContext.BaseDirectory;
+if (args.Contains(ARG_WAIT_FOR_DEBUGGER))
+{
+	Debugger.Launch();
+}
+
+string selfDirectory = AppContext.BaseDirectory;
+string root = Path.GetFullPath(Path.Combine(selfDirectory, "../.."));
 
 if (! args.Contains(ARG_NO_HEADER))
 {
-	var name = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
-	var file = Path.Combine(root, $"{name}.exe");
+	var file = Process.GetCurrentProcess().MainModule.FileName;
 	var version = FileVersionInfo.GetVersionInfo(file).ProductVersion;
 
 	Console.WriteLine
 	(
-		"NSO Uninstaller"
+		"Mod Uninstaller"
 		+ "\nCopyright 2024 teunu, CodeLeopard"
 		+ "\nThis program comes with ABSOLUTELY NO WARRANTY"
 		//+ "\nLicense: LGPL-3.0-or-later See: https://www.gnu.org/licenses/" // todo: pick license and add to repo.
@@ -47,13 +54,25 @@ var stormworksExepath = Path.Combine(root, "stormworks.exe");
 var stormworksExepath64 = Path.Combine(root, "stormworks64.exe");
 if (!File.Exists(stormworksExepath) && !File.Exists(stormworksExepath64))
 {
-	Console.WriteLine($"Did not find '{stormworksExepath}' unable to uninstall NSO.".Pastel(ConsoleColor.Red));
-	Console.WriteLine("The uninstaller should be placed in the stormworks installation folder, next to 'stormworks.exe'.".Pastel(ConsoleColor.Red));
+	Console.WriteLine($"Did not find '{stormworksExepath}' unable to uninstall.".Pastel(ConsoleColor.Red));
+	Console.WriteLine("The uninstaller should be placed in YOUR_STORMWORKS_INSTALLATION_FOLDER/rom/MOD_NAME.".Pastel(ConsoleColor.Red));
 	Finish();
 	return 1;
 }
 
-Console.WriteLine($"About to uninstall NSO from: {root}");
+
+var modInfoFile = Path.Combine(selfDirectory, "files.txt");
+if (!File.Exists(modInfoFile))
+{
+	Console.WriteLine($"Did not find '{modInfoFile}' unable to uninstall.".Pastel(ConsoleColor.Red));
+	Console.WriteLine("The uninstaller should be placed in YOUR_STORMWORKS_INSTALLATION_FOLDER/rom/MOD_NAME.".Pastel(ConsoleColor.Red));
+	Finish();
+	return 1;
+}
+
+var modInfo = ModInfo.Parse(modInfoFile);
+
+Console.WriteLine($"About to uninstall {modInfo.full_name} from: {root}");
 
 if (args.Contains(ARG_ONLY_STARTUP))
 {
@@ -65,7 +84,7 @@ if (args.Contains(ARG_ONLY_STARTUP))
 if (!skipConfirmation && !Confirm("Continue with uninstall?"))
 {
 	Console.ForegroundColor = ConsoleColor.DarkYellow;
-	Console.WriteLine("NSO was not uninstalled");
+	Console.WriteLine($"{modInfo.short_name} was not uninstalled");
 	Console.ResetColor();
 	Finish();
 	return 1;
@@ -77,20 +96,11 @@ Finish();
 return 0;
 
 
+
 void Uninstall()
 {
-	// The script that generates the installation zip file also creates an inventory of files.
-	// We simply delete all the files mentioned there.
-	// Note: If an update to NSO stops including a file it would be left behind by the uninstaller, so the updater should remove it at update time.
-	// Note: This file is also on the list to be deleted, so we have to read it all and close it before starting.
-	var lines = File.ReadAllLines(Path.Combine(root, "rom/nso_mod/files.txt"));
-	foreach (var line in lines)
+	foreach (var line in modInfo.file_list)
 	{
-		if (string.IsNullOrWhiteSpace(line) || line.StartsWith('#'))
-		{
-			continue;
-		}
-
 		var finalPath = Path.Combine(root, line);
 
 		try
@@ -120,7 +130,7 @@ void Uninstall()
 		}
 	}
 
-	Console.WriteLine("NSO related files deleted.".Pastel(ConsoleColor.DarkGreen));
+	Console.WriteLine($"{modInfo.short_name} related files deleted.".Pastel(ConsoleColor.DarkGreen));
 
 	Console.ResetColor();
 	Console.WriteLine("Automatic part finished; Please check file integrity in steam");
@@ -157,7 +167,9 @@ void Uninstall()
 	}
 
 	Console.ForegroundColor = ConsoleColor.Green;
-	Console.WriteLine("NSO has been uninstalled successfully! Happy storming!");
+	Console.WriteLine($"{modInfo.short_name} has been uninstalled successfully!");
+	Console.WriteLine($"Don't forget to also remove the Uninstaller after closing it.");
+	Console.WriteLine("Happy storming!");
 }
 
 void Finish()
